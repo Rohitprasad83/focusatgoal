1e0c6cdca6d15c78d14e3995cebe66f58362fc95
Rohit Prasad <pdrohit674@gmail.com>
2022-05-14 15:06:16 +0530

    feat: added pages to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import logo from './logo.svg'
+import wallpapers from './db/images'
+import { UserOnboarding, Homepage } from './pages/'
+import './App.css'
 
 function App() {
+  const [wallpaper, setWallpaper] = useState(wallpapers[0])
+  const [userDetails, setUserDetails] = useState(null)
+  const name = localStorage.getItem('name')
+  const city = localStorage.getItem('city')
+  useEffect(() => {
+    setWallpaper(wallpapers[Math.floor(Math.random() * 10)])
+
+    if (name && city) {
+      setUserDetails(true)
+    }
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App font-mono text-slate-100"
+      style={{
+        backgroundImage: `url("${wallpaper}")`,
+        backgroundSize: 'cover',
+      }}>
+      {userDetails ? (
+        <Homepage name={name} city={city} />
+      ) : (
+        <UserOnboarding setUserDetails={setUserDetails} />
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
