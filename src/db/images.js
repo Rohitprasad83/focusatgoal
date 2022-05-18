6afccedb0355b0e5c8cc84f3f26938c5672b82e8
Rohit Prasad <pdrohit674@gmail.com>
2022-05-19 00:52:30 +0530

    feat: added new background photos

--- src/db/images.js
+++ src/db/images.js
@@ -9,6 +9,19 @@ import img8 from '../assets/img8.jpg'
 import img9 from '../assets/img9.jpg'
 import img10 from '../assets/img10.jpg'
 import img11 from '../assets/img11.jpg'
+import img12 from '../assets/img12.jpg'
+import img13 from '../assets/img13.jpg'
+import img14 from '../assets/img14.jpg'
+import img15 from '../assets/img15.jpg'
+import img16 from '../assets/img16.jpg'
+import img17 from '../assets/img17.jpg'
+import img18 from '../assets/img18.jpg'
+import img19 from '../assets/img19.jpg'
+import img20 from '../assets/img20.jpg'
+import img21 from '../assets/img21.jpg'
+import img22 from '../assets/img22.jpg'
+import img23 from '../assets/img23.jpg'
+import img24 from '../assets/img24.jpg'
 
 const wallpapers = [
     img1,
@@ -22,6 +35,19 @@ const wallpapers = [
     img9,
     img10,
     img11,
+    img12,
+    img13,
+    img14,
+    img15,
+    img16,
+    img17,
+    img18,
+    img19,
+    img20,
+    img21,
+    img22,
+    img23,
+    img24,
 ]
 
 export default wallpapers
