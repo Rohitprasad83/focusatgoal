c12c9d140bc376260bf36e83eac9660fce79ad81
Rohit Prasad <pdrohit674@gmail.com>
2022-05-21 14:28:31 +0530

    feat: changed weather api call from city to latitude and longitude

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -4,23 +4,33 @@ import axios from 'axios'
 export function Weather() {
   const [temperature, setTemperature] = useState(0)
   const apiKey = '79c0baacc3b6f750ac3ddfc13986a423'
-  const city = localStorage.getItem('city')
+  const [location, setLocation] = useState('')
+
+  const [lat, setLat] = useState('28.6139')
+  const [long, setLong] = useState('77.2090')
 
   useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      setLat(position.coords.latitude)
+      setLong(position.coords.longitude)
+    })
     ;(async () => {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`
       const response = await axios.get(url)
       setTemperature(Number(response.data.main.temp) - 273)
+      setLocation(response.data.name)
     })()
-  }, [])
+  }, [lat, long, location])
 
   return (
-    <div className="flex-col items-center absolute text-2xl top-5 right-5 drop-shadow-2xl">
-      <div className="flex">
-        <span className="my-auto">{temperature.toFixed(1)}° C</span>
+    <div className="flex-col items-center absolute text-2xl top-5 right-0">
+      <div className="flex flex-col">
+        <span className="my-auto items-center">
+          {temperature.toFixed(1)}° C
+        </span>
       </div>
 
-      <div className="absolute right-2">{city}</div>
+      <div className="w-36 text-lg">{location}</div>
     </div>
   )
 }
